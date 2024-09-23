4c70cf85aae1d8b97623b34beeb4c396bb3d304e
Sfsprecher <sfsprecher@miners.utep.edu>
2024-09-23 12:42:20 -0600

    9/23

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+bm = pd.read_csv("big-mac-full-index.csv")
+
+# for index, row in bm.iterrows():
+#     print(row["date"], row["iso_a3"], row["currency_code"])
+
+print(bm.apply(lambda row: row["date"], axis = 1))
